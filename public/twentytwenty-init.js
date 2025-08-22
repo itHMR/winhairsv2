b07618dc473dafde912f740246475adc2fc0412e
David Diaz <it@hairmedicalrestoration.com>
2025-08-22 14:57:10 -0700

    Landing con Astro de Winhairs v2

---
+++ public/twentytwenty-init.js
@@ -0,0 +1,8 @@
+// public/twentytwenty-init.js
+window.addEventListener("DOMContentLoaded", function () {
+  $(".twentytwenty-container").twentytwenty({
+    default_offset_pct: 0.5,
+    before_label: "Before",
+    after_label: "After"
+  });
+});

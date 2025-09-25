4d09421403732b1744e4286526574ee375068fb0
David Diaz <it@hairmedicalrestoration.com>
2025-09-25 11:45:03 -0700

    Se subieron nuevas imagenes para el slider

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  //output: 'static', // 👈 MUY IMPORTANTE: le dice a Astro que genere archivos HTML estáticos
   vite: {
     plugins: [tailwindcss()]
   }

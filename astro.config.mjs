7215a8c76fdf1f6df4f06877fd9455a811029612
David Diaz <it@hairmedicalrestoration.com>
2025-11-10 11:39:22 -0700

    Se agregaron scripts de Google Ads y Google Search console, ademas de los sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   //output: 'static', // 👈 MUY IMPORTANTE: le dice a Astro que genere archivos HTML estáticos
+  site: 'https://winhairtj.com', // cambia por tu dominio real
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
